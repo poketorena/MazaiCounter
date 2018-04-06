9827823cddc7135f988c34ec7b4a7864d8c36ad9
poketorena <poketorena@users.noreply.github.com>
2018-04-07 00:26:54 +0900

    Converterを追加

---
+++ MazaiCounter/MazaiCounter/Converters/IntToMazaiTypeDeconverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using System.Globalization;
+using MazaiCounter.Models;
+
+namespace MazaiCounter.Converters
+{
+    class IntToMazaiTypeDeconverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (value is int num)
+            {
+                return (MazaiType)Enum.ToObject(typeof(MazaiType), num);
+
+            }
+            throw new ArgumentException();
+        }
+    }
+}
---
+++ MazaiCounter/MazaiCounter/Converters/MazaiTypeToStringConverter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+using Xamarin.Forms;
+using MazaiCounter.Models;
+
+namespace MazaiCounter.Converters
+{
+    public class MazaiTypeToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return "NullError";
+            }
+
+            if (value is MazaiType type)
+            {
+                switch (type)
+                {
+                    case MazaiType.RedBull:
+                        {
+                            return "RedBull";
+                        }
+
+                    case MazaiType.Monster:
+                        {
+                            return "Monster";
+                        }
+
+                    case MazaiType.MonsterAbsolutelyZero:
+                        {
+                            return "Monster Absolutely Zero";
+                        }
+
+                    case MazaiType.MonsterKhaos:
+                        {
+                            return "Monster KHAOS";
+                        }
+
+                    case MazaiType.MonsterTheDoctor:
+                        {
+                            return "Monster THE DOCTOR";
+                        }
+
+                    default:
+                        {
+                            return "ArgumentError";
+                        }
+                }
+            }
+            else
+            {
+                return "TypeError";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
